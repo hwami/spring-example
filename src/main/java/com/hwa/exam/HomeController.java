1f13a785af16bc6d521db55185ef1086ca4c3899
myki <myki@DESKTOP-DI8HLF5>
2019-03-29 17:53:01 +0900

    init project

---
+++ src/main/java/com/hwa/exam/HomeController.java
@@ -0,0 +1,39 @@
+package com.hwa.exam;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	protected Logger log = LogManager.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		log.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+}
---
+++ src/main/java/com/hwa/exam/common/LoggerInterceptor.java
@@ -0,0 +1,33 @@
+package com.hwa.exam.common;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class LoggerInterceptor extends HandlerInterceptorAdapter {
+	protected Logger log = LogManager.getLogger(LoggerInterceptor.class);
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		if (log.isDebugEnabled()) {
+			log.debug("\n");
+			log.debug("====================================== START ======================================");
+			log.debug(" Request URI \t: " + request.getRequestURI());
+		}
+		return super.preHandle(request, response, handler);
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		if (log.isDebugEnabled()) {
+			log.debug("====================================== END ======================================\n");
+		}
+	}
+
+}
